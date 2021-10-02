c4b7e14e337692f0d21aa21d5f36524ca38ba333
Regis Freyd <djaiss@users.noreply.github.com>
2021-10-02 17:22:48 -0400

    Update one-on-one.blade.php

--- source/features/one-on-one.blade.php
+++ source/features/one-on-one.blade.php
@@ -4,19 +4,19 @@
 <div class="container mx-auto w-full max-w-4xl mb-14">
   <h3 class="mb-8 text-center text-5xl">1:1 management</h3>
 
-  <p class="mb-16 max-w-prose text-center mx-auto text-xl">One on ones between a manager and her direct reports are critical for both parties. It's a privileged moment to understand what's going on, to discuss challenges and to talk about how to grow. OfficeLife makes this entire process super easy and actually fun.</p>
+  <p class="mb-16 max-w-prose text-center mx-auto text-xl">One on ones between manager and employees are critical for both parties. It's a privileged moment to understand what's going on, to discuss challenges and to talk about how to grow. OfficeLife makes this entire process super easy and actually fun.</p>
 
   <h3 class="mb-8 text-center text-3xl font-bold">Anatomy of a 1:1.</h3>
 
-  <p class="mb-8 max-w-prose text-center mx-auto text-xl"></p>
+  <p class="mb-8 max-w-prose text-center mx-auto text-xl">As soon as an employee is assigned a manager, both she and her manager will find on the dashboard a place to participate to 1:1. A 1:1 in OfficeLife has 3 main sections, that match what people talk about in real life: talking points, action items that are defined when the discussion takes place, and notes. Both the employee and the manager can populate a 1:1 either in advance or during the meeting. And when the meeting is over, simply mark it as happened, and a fresh entry will be created for your next session.</p>
 
   @component('_components.screenshot')
-  379403fc-f74e-4fe8-8f09-2842a1a5a0ef
+  b849ef39-9ac3-472d-ad03-2faae16bec70
   @endcomponent
 
-  <h3 class="mt-32 mb-8 text-center text-3xl font-bold">Know what your team mates have done at a glance.</h3>
+  <h3 class="mt-32 mb-8 text-center text-3xl font-bold">Browse past entries.</h3>
 
-  <p class="mb-16 max-w-prose text-center mx-auto	text-xl">When the employee is part of a team, (s)he can read what other have done on the team's dashboard. OfficeLife provides a simple way of browsing the current week, as well as last Friday, and see who's done what. And of course, all the work logs are also available for everyone to see on the profile page of the employee.</p>
+  <p class="mb-16 max-w-prose text-center mx-auto	text-xl">Both the manager and the employee can see past 1:1s very easily. Also, even the employee has a new manager, she will be able to see her past 1:1 on her profile. It's, obviously, totally private.</p>
 
   @component('_components.screenshot')
   ed41da08-d522-46f5-95d4-32e7613c068e
